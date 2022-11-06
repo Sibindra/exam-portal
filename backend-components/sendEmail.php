a551cce8b462f33ee5e5ee03d58db1b807dcc270
Sibindra <timalsinasibindra@gmail.com>
2022-11-07 02:58:05 +0545

    php mailder code complete

--- backend-components/sendEmail.php
+++ backend-components/sendEmail.php
@@ -14,9 +14,7 @@ if (!$conn) {
     die("Connect failed:");
 } else {
 
-    // echo("sucess");
-    // $to_email=$_POST['email'];
-    $to_mail="timalsinasibindra@gmail.com";
+    $to_email=$_POST['email'];
 
     // generate 4 digit code
     $code = mt_rand(1111,9999);
@@ -32,7 +30,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'examportalnepal@gmail.com';                     //SMTP username
-    $mail->Password   = 'tGPgu9hmKcEenmk';                               //SMTP password
+    $mail->Password   = 'lysdugovoqhgolqg';                               //SMTP password
     $mail->SMTPSecure = "tls";            //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
@@ -43,47 +41,18 @@ try {
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Here is the subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+    $mail->Subject = 'exam portal verification code';
+    $mail->Body    = "Your verification code is:'$code' ";
 
     $mail->send();
     echo 'Message has been sent';
+
+    header('Location: ../Password/pw.php');
+    exit;
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
     
-
-    // // new php mailer class
-    // $mail = new PHPMailer();
-
-    // require "phpmailer/scr/Exception.php";
-    // require "phpmailder/src/PHPMailer.php";
-    // require "phpmailer/src/SMTP.php";
-
-    // $mail=new PHPMailer(true);
-
-    // $mail->isSMTP();
-    // $mail->Host="smtp.gmail.com";
-    // $mail->SMTPAuth=true;
-    // $mail->Username='examportalnepal@gmail.com';
-    // $mail->Password='lysdugovoqhgolqg';
-    // $mail->SMTPSecure="ssl";
-    // $mail->Port=465;
-
-    // $mail->setFrom("examportalnepal@gmail.com");
-
-    // $mail->addAddress($to_mail);
-
-    // $mail->isHTML(true);
-
-    // $mail->Subject="Exam Portal test mail";
-
-    // $mail->Body="Your code is :'$code'";
-
-    // $mail->send();
-
-    // echo "works";
 
 }
 
