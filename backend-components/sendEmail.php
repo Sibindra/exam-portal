67d20336b44cda7d95421761253127a5e8909bd0
Sibindra <timalsinasibindra@gmail.com>
2022-11-07 02:43:55 +0545

    php mailer works but smtp error

--- backend-components/sendEmail.php
+++ backend-components/sendEmail.php
@@ -1,7 +1,6 @@
 <?php
-
-
-
+// php mailer dependencies
+use PHPMailer\PHPMailer\PHPMailer;
 
 // connection to localhost
 $server = "localhost";
@@ -9,130 +8,83 @@ $username = "root";
 $password = "";
 $dB = "Exam-Portal";
 
-$con = mysqli_connect($server, $username, $password, $dB);
+$conn = mysqli_connect($server, $username, $password, $dB);
 
-if (!$con) {
+if (!$conn) {
     die("Connect failed:");
 } else {
-    // echo "Sucess";
 
-}
-
-
+    // echo("sucess");
     // $to_email=$_POST['email'];
-    $to_email="timalsinasibindra@gmail.com";
+    $to_mail="timalsinasibindra@gmail.com";
 
-    // echo $email;
-
+    // generate 4 digit code
     $code = mt_rand(1111,9999);
+   
+    require 'vendor/autoload.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'examportalnepal@gmail.com';                     //SMTP username
+    $mail->Password   = 'tGPgu9hmKcEenmk';                               //SMTP password
+    $mail->SMTPSecure = "tls";            //Enable implicit TLS encryption
+    $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('examportalnepal@gmail.com', 'Exam-Portal');
+    $mail->addAddress($to_mail);               //Name is optional
+
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
     
 
-    // $subject = "Simple Email Test via PHP";
-    // $body = "Hi, This is test email send by PHP Script";
-    // $headers = "From: sender email";
+    // // new php mailer class
+    // $mail = new PHPMailer();
 
-    // if (mail($to_email, $subject, $body, $headers)) {
-    //     echo "Email successfully sent to $to_email...";
-    // } else {
-    //     echo "Email sending failed...";
-    // }
-
-    // use PHPMailer\PHPMailer\PHPMailer;
-    // // use PHPMailer\PHPMailer\Exception;
-    // require 'path/to/PHPMailer/src/Exception.php';
-    // require 'path/to/PHPMailer/src/PHPMailer.php';
-    // require 'path/to/PHPMailer/src/SMTP.php';
-
+    // require "phpmailer/scr/Exception.php";
+    // require "phpmailder/src/PHPMailer.php";
+    // require "phpmailer/src/SMTP.php";
 
     // $mail=new PHPMailer(true);
-
 
     // $mail->isSMTP();
     // $mail->Host="smtp.gmail.com";
     // $mail->SMTPAuth=true;
-    // $mail->Username="examportalnepal@gmail.com";
-    // $mail->Password="lysdugovoqhgolqg";
-
-    // $mail->SMTPSecure='ssl';
+    // $mail->Username='examportalnepal@gmail.com';
+    // $mail->Password='lysdugovoqhgolqg';
+    // $mail->SMTPSecure="ssl";
     // $mail->Port=465;
 
-    // $mail->setForm("examportalnepal@gmail.com");
+    // $mail->setFrom("examportalnepal@gmail.com");
 
-    // $mail->addAddress("timalsinasibindra@gmail.com");
+    // $mail->addAddress($to_mail);
 
     // $mail->isHTML(true);
 
-    // $mail->Subject="exam portal verification code";
+    // $mail->Subject="Exam Portal test mail";
 
-    // $mail->Body="Your verification code is :'$code'";
-    
+    // $mail->Body="Your code is :'$code'";
+
     // $mail->send();
 
+    // echo "works";
 
-    // use PHPMailer\PHPMailer\PHPMailer;
-
-    // require 'vendor/autoload.php';
-
-    // $mail = new PHPMailer;
-
-    // $mail->isSMTP();
-
-    // $mail->SMTPDebug = 2;
-
-    // $mail->Host = 'smtp.hostinger.com';
-    // $mail->Port = 587;
-
-    // $mail->SMTPAuth = true;
-
-    // $mail->Username = 'examportalnepal@gmail.com';
-    // $mail->Password = 'lysdugovoqhgolqg';
-
-    // $mail->setFrom('examportalnepal@gmail.com', 'Exam Portal');
-    // $mail->addReplyTo('examportalnepal@gmail.com', 'Exam Portal');
-
-    // $mail->addAddress('timalsinasibindra@gmail.com', 'Hello');
-
-    // $mail->Subject = 'Checking if PHPMailer works';
-
-    // $mail->msgHTML(file_get_contents('message.html'), __DIR__);
-    // $mail->Body = 'This is just a plain text message body';
-    // //$mail->addAttachment('attachment.txt');
-    // if (!$mail->send()) {
-    //     echo 'Mailer Error: ' . $mail->ErrorInfo;
-    // } else {
-    //     echo 'The email message was sent.';
-    // }
-
-
-  
-    require 'vendor/autoload.php';
-  
-    require_once '../class.phpmailer.php';
-
-    $mail = new PHPMailer(true);
-  
-try {
-    $mail->SMTPDebug = 2;                                       
-    $mail->isSMTP();                                            
-    $mail->Host       = 'smtp.gfg.com;';                    
-    $mail->SMTPAuth   = true;                             
-    $mail->Username   = 'examportalnepal@gmail.com';                 
-    $mail->Password   = 'lysdugovoqhgolqg';                        
-    $mail->SMTPSecure = 'tls';                              
-    $mail->Port       = 587;  
-  
-    $mail->setFrom('examportalnepal@gmail.com', 'Name');           
-    $mail->addAddress('timalsinasibindra@gmail.com');
-    $mail->addAddress('timalsinasibindra@gmail.com', 'Name');
-       
-    $mail->isHTML(true);                                  
-    $mail->Subject = 'Subject';
-    $mail->Body    = 'HTML message body in <b>bold</b> ';
-    $mail->AltBody = 'Body in plain text for non-HTML mail clients';
-    $mail->send();
-    echo "Mail has been sent successfully!";
-} catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
 ?>
